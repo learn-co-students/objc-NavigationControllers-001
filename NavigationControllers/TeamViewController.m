74ea598ccad2baa7cbceb8678c42e43dba56f586
John Richardson <richardsonjm@gmail.com>
2016-06-02 12:20:39 -0400

    Send details if segue is al

--- NavigationControllers/TeamViewController.m
+++ NavigationControllers/TeamViewController.m
@@ -7,7 +7,17 @@
 //
 
 #import "TeamViewController.h"
+#import "TeamDetailViewController.h"
 
 @implementation TeamViewController
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue
+                 sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"al"]) {
+        TeamMember *al = [[TeamMember alloc] initWithname:@"Al" phoneNumber:@"5551212" birthCity:@"Detroit" birthState:@"Michigan" favoriteBand:@"The Beatles"];
+        TeamDetailViewController *TDVC = [segue destinationViewController];
+        TDVC.teamMember = al;
+    }
+}
 
 @end
