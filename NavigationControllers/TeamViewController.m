cb6f6a5e206e0aedb039093e200905e17b5583a8
John Richardson <richardsonjm@gmail.com>
2016-06-02 13:34:55 -0400

    Build out rest of team options

--- NavigationControllers/TeamViewController.m
+++ NavigationControllers/TeamViewController.m
@@ -13,10 +13,19 @@
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue
                  sender:(id)sender {
+    TeamDetailViewController *TDVC = [segue destinationViewController];
     if ([segue.identifier isEqualToString:@"al"]) {
         TeamMember *al = [[TeamMember alloc] initWithname:@"Al" phoneNumber:@"5551212" birthCity:@"Detroit" birthState:@"Michigan" favoriteBand:@"The Beatles"];
-        TeamDetailViewController *TDVC = [segue destinationViewController];
         TDVC.teamMember = al;
+    } else if ([segue.identifier isEqualToString:@"joe"]) {
+        TeamMember *joe = [[TeamMember alloc] initWithname:@"Joe" phoneNumber:@"8675309" birthCity:@"Sing Sing" birthState:@"New York" favoriteBand:@"The Who"];
+        TDVC.teamMember = joe;
+    } else if ([segue.identifier isEqualToString:@"chris"]) {
+        TeamMember *chris = [[TeamMember alloc] initWithname:@"Chris" phoneNumber:@"9999999" birthCity:@"Cleveland" birthState:@"Ohio" favoriteBand:@"The Cult"];
+        TDVC.teamMember = chris;
+    } else if ([segue.identifier isEqualToString:@"avi"]) {
+        TeamMember *avi = [[TeamMember alloc] initWithname:@"Avi" phoneNumber:@"5555555" birthCity:@"New York" birthState:@"New York" favoriteBand:@"The Band"];
+        TDVC.teamMember = avi;
     }
 }
 
